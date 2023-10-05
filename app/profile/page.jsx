0095e71cf7efec8e48ec4794e4eae23f0e24659c
Dmytro Petyshyn <dev6012@gmail.com>
2023-10-05 19:31:52 +0300

    vercel

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
-import Profile from '@/components/profile';
+import Profile from '@/components/Profile';
 
 const MyProfile = () => {
   const { data: session } = useSession();
